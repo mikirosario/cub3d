4d438ea4cc2a174472f767feafc2850afffab867
Michael Rosario <mrosario@student.42madrid.com>
2020-09-08 23:18:15 +0200

    I survived thin walls. -2020

--- graphicsengine/dda_bonus.c
+++ graphicsengine/dda_bonus.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   dda_bonus.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrosario <mrosario@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mikiencolor <mikiencolor@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/11 18:13:54 by mrosario          #+#    #+#             */
-/*   Updated: 2020/09/08 20:35:01 by mrosario         ###   ########.fr       */
+/*   Updated: 2020/09/08 23:14:59 by mikiencolor      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d_bonus.h"
 
-int		hitdoor(void)
+
+int		verticaldoor(void)
 {
-	
 	//double	sidedistx;
 	//double	sidedisty;
 	double	mapx;
@@ -59,8 +59,12 @@ int		hitdoor(void)
 		door.starty = 0.5;
 		door.endx = 1;
 		door.endy = 0.5;
-		if (findintersection(&ray, &door, &x, &y))
-			if ((x > 0 && x < 1) && y == 0.5)
+		(void)y;
+		(void)x;
+		(void)door;
+		//if (findintersection(&ray, &door, &x, &y))
+			//if ((x > 0 && x < 1))
+			if ((ray.endy >= 0.5 && g_raydata.stepy > 0) || (ray.endy <= 0.5 && g_raydata.stepy <= 0))
 			{
 				raytodoor.opposite = 0.5;
 				raytodoor.anglealpha = PI/2;
@@ -110,6 +114,93 @@ int		hitdoor(void)
 }
 
 /*
+** This function modifies the raycaster code a bit to cast 'thin' walls that
+** are painted across the middle of the square instead of the edge, which
+** I will (eventually, finally, after several days of banging my head against
+** the table :_( xD) be using as Wolfenstein-style sliding doors.
+**
+** To do this I must alter the ray logic a bit. Now, if a ray intersects a line
+** halfway up the square representing the door, it is a special door hit, and
+** will return 1, causing the calling function to return 2 instead of 1, which
+** will be used as a signal to render the pixel stripe at a distance of 0.5
+** further away from the player than the usual map square edge, as well as
+** change the texture, check the door state, etc.
+**
+** If it does NOT intersect the door line it will return 0 and the normal dda
+** code will handle the ray.
+**
+** For the moment I don't handle the horizontal casting for this, so it will
+** interpret the neighbouring floor square as 'overlapping' the wall if
+** next to a wall... It's not a bug, it's a feature! xD Meaning I'll get to it.
+** Eventually I'll draw an H, when I'm over the sheer trigonometric trauma I've
+** been through over the past few days. xD
+** 
+** This function is for doors perpendicular to the y axis (horizontal doors,
+** visible from above and below), so it is only called if side == 1, which
+** tells us that we hit the edge of a map square from tbe 'bottom' (or 'top' if
+** you prefer, it's relative, that is, the y vector, or side facing the front
+** of the player).
+**
+** Knowing that, I ask again whether sidedistx is less than sidedisty, which
+** tells me whether the *next* map square this ray will encounter is on the
+** side (x vector) or 'top' (y vector). If sidedistx is less than sidedisty
+** then I know that we will leave this map square from the side (x vector). If
+** it is not, then I know that we will leave this map square from the
+** 'top' (y vector).
+**
+** Since I also know that we *entered* from the 'bottom', if we leave from the
+** 'top' this means we MUST have intersected the middle line along the way, so
+** that is easy. If sidedisxt is not less than sidedist y, we definitely hit
+** the door, always, and return 1.
+**
+** If we will leave from the side, however, it will depend on the ray's angle.
+** if we leave below the position of the door on the y axis (0.5 over the map
+** square edge), then we do not hit the wall and so return 0. If we leave at
+** or above the door position on the y axis, then we hit the door and return
+** 1.
+**
+** To determine whether we hit or missed, I repurpose the equations used to get
+** wallx for texture caster (the position *where* we hit the next square edge).
+** Confusingly, if we leave following the x vector and hit next square on the
+** side, wallx is actually a y coordinate. :p I've never liked the name wallx
+** but I kept it out of respect for the Lode tutorial, because where would I
+** be without it! xD
+**
+** Anyway, if we calculate wallx as if we were going to draw that wall, and
+** then subtract it from the floor of itself (to keep only the decimals), we
+** have an absolute y value for where we leave the square we are in. If we are
+** facing south (stepy > 0) then it is a hit if that value is 0.5 or greater.
+** If we are facing north (stepy < 0), then it is a hit if that value is 0.5 or
+** less.
+**
+** Yes... I made a line intersect function. I made a hypotenuse function. I was
+** about to write a thesis on trigonometry. In the end... well they're still
+** pretty cool functions. xD But I don't need them here. Into libft they go.
+*/
+
+int		hordoorhitcheck(void)
+{
+	double	mapx;
+	double perpwalldist;
+	double	wallx;
+
+//we came from below and exit through the side, maybe we hit the door, maybe not
+	if (g_raydata.sidedistx < g_raydata.sidedisty)
+	{	
+		mapx = g_raydata.mapx + g_raydata.stepx; //go to next square
+		perpwalldist = (mapx - g_player.posx + \
+		(1 - g_raydata.stepx) / 2) / g_raydata.raydirx; //calculate perpendicular distance to player camera
+		wallx = g_player.posy + perpwalldist * g_raydata.raydiry; //calculate where on the adjacent square edge the ray hit, this will effectively be a y axis value in our square
+		wallx -= floor(wallx); //remove the map-centric component of wallx and get the square-centric component
+		if ((wallx >= 0.5 && g_raydata.stepy > 0) || (wallx <= 0.5 && g_raydata.stepy <= 0))
+			return (1);
+	}
+	else //if we came from below and are exiting from above, we definiely hit the door
+			return (1);
+	return (0);
+}
+
+/*
 ** The hit variable will be initialized to 0 here, and will later be used to
 ** record a wall hit. Until a wall is hit, we'll keep moving the ray from
 ** side to side, where walls can be. Depending on the ray's orientation, it
@@ -137,8 +228,8 @@ void	sidetoside(void)
 		if (g_config.map[g_raydata.mapy][g_raydata.mapx] == '1')
 			g_raydata.hit = 1;
 		if (g_config.map[g_raydata.mapy][g_raydata.mapx] == '_' && g_raydata.side == 1)
-				if (hitdoor())
-					g_raydata.hit = 2;
+			if (hordoorhitcheck())
+				g_raydata.hit = 2;
 				//if (g_raydata.sidedisty - g_raydata.deltadisty < g_raydata.mapy + g_raydata.stepy / 2)
 				//	g_raydata.hit = 2;
 	}
